294c960200eb27ac3e4b9d1506c54ed1913e51fe
fhizzz27 <muhammadhafizudin1212@gmail.com>
2024-02-28 16:29:40 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,89 @@
+const audioIcon = document.querySelector('.audio-icon-wrapper i');
+const stickyTop = document.querySelector('.sticky-top');
+const offcanvas = document.querySelector('.offcanvas');
+const rootElement = document.querySelector(":root");
+const audioIconWrapper = document.querySelector('.audio-icon-wrapper');
+let isPlaying = false;
+const song = document.querySelector('#song');
+
+offcanvas.addEventListener('show.bs.offcanvas', function() {
+    stickyTop.style.overflow ='visible';
+});
+
+offcanvas.addEventListener('hidden.bs.offcanvas', function(){
+    stickyTop.style.overflow = 'hidden';
+});
+
+
+function disableScroll() {
+    const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    const scrollLeft = window.pageXOffset || document.documentElement.scrollLeft;
+
+
+    window.onscroll = function () {
+        window.scrollTo(scrollTop, scrollLeft);
+    }
+}
+function enableScroll() {
+    window.onscroll = function () {}
+    rootElement.style.scrollBehavior = 'smooth';
+    // localStorage.setItem('opened', 'true');
+    playAudio();
+}
+function playAudio() {
+  song.volume = 0.1;
+  audioIconWrapper.style.display = 'flex';
+  song.play();
+  isPlaying = true;
+}
+
+
+audioIconWrapper.onclick = function() {
+  if (isPlaying) {
+    song.pause();
+    audioIcon.classList.remove('bi-disc');
+    audioIcon.classList.add('bi-pause-circle');
+  } else {
+    song.play();
+    audioIcon.classList.add('bi-disc');
+    audioIcon.classList.remove('bi-pause-circle');
+  }
+
+
+  isPlaying = !isPlaying;
+}
+
+
+// if(!localStorage.getItem('opened')) {
+//     
+// }
+disableScroll();
+
+window.addEventListener("load", function() {
+    const form = document.getElementById('my-form');
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      const data = new FormData(form);
+      const action = e.target.action;
+      fetch(action, {
+        method: 'POST',
+        body: data,
+      })
+      .then(() => {
+        alert("Konfirmasi Kehadiran terkirim.....");
+      })
+    });
+  });
+
+
+  // ?p=Ibu&n=Zakiyah
+const urlParams = new URLSearchParams(window.location.search);
+const nama = urlParams.get('n') || '';
+const pronoun = urlParams.get('p') || 'Bapak/Ibu/Saudara/i, ';
+console.log(nama)
+const namaContainer = document.querySelector('.hero h4 span');
+namaContainer.innerText = `${pronoun} ${nama},`.replace(/ ,$/, '');
+
+document.querySelector('#nama.').value = nama;
+
+
